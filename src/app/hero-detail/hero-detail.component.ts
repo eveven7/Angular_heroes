b2455f51375bf8cdf28889a96c9b11d1fdd63545
Evelina <venckute.evelina1.com>
2023-04-02 17:57:47 +0300

    Added dashboard

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroService } from '../service/hero.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
+
+  constructor(private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location) { }
+
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void{
+    this.location.back();
+  }
+  ngOnInit(): void {
+    this.getHero();
+  }
+
 }
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -19,4 +19,10 @@ export class HeroService {
   }
 
   constructor(private messageService: MessageService) { }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(hero => hero.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
