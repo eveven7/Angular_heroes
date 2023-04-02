89073bf8ff3e78e3ed5c1c9b5e4dd83f4f150f7e
Evelina <venckute.evelina1.com>
2023-04-02 11:12:23 +0300

    Updated HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../service/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +10,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
+
+constructor(private heroService: HeroService){}
  heroes = HEROES;
  selectedHero?: Hero;
+ heroes$!: Observable<Hero[]>;
 
  onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
+
+ngOnInit(): void {
+ this.heroes$ = this.heroService.getHeroes();
+}
+
 }
