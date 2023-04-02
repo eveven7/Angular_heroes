e89b278f20865e7a4e4473170306e181f21f8a99
Evelina <venckute.evelina1.com>
2023-04-02 12:40:47 +0300

    Working MessageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../service/hero.service';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,19 +11,22 @@ import { HeroService } from '../service/hero.service';
   styleUrls: ['./heroes.component.css']
 
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
 
-constructor(private heroService: HeroService){}
- heroes = HEROES;
- selectedHero?: Hero;
- heroes$!: Observable<Hero[]>;
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  heroes = HEROES;
+  selectedHero?: Hero;
+  heroes$!: Observable<Hero[]>;
 
- onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`Heroes Component: Selected hero id=${hero.id}`);
+  }
 
-ngOnInit(): void {
- this.heroes$ = this.heroService.getHeroes();
-}
+  ngOnInit(): void {
+    this.heroes$ = this.heroService.getHeroes();
+  }
+
+
 
 }
